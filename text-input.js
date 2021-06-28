3a6d9329eeff1a7fa3407338a5101a2d06a461e1
leirisyue <leirisyue@gmail.com>
2021-06-28 15:23:28 +0700

    create

---
+++ text-input.js
@@ -0,0 +1,40 @@
+//
+document.getElementById("title_job").innerHTML = "Mục tiêu nghề nghiệp";
+document.getElementById("job").innerHTML = "Tôi mong muốn tìm được một môi trường làm việc thân thiện, có cơ hội để thể hiện những kiến thức, kỹ năng và hoàn thiện bản thân. Từng bước nâng cao kiến thức chuyên môn lập trình và hoàn thành tốt công việc. Hơn thế nữa là vai trò một thành viên trong công ty có khả năng thăng tiến.";
+//
+document.getElementById("title_education").innerHTML = "Học vấn";
+document.getElementById("education").innerHTML =
+    "<span class='text-title2'>Đại học khoa học tự nhiên - dhqg tp.hcm</br></span>" +
+    "<span class='text-detail'>Khoa Khoa học Môi trường; chuyên ngành tin học môi trường.</br></span>" +
+    "<span class='text-detail'>Khoa Công nghệ thông tin; chương trình văn bằng hai.</br></span>" +
+    "<span class='text-title2'>trung tâm tin học đại học khoa học tự nhiên - tphcm.</br></span>" +
+    "<span class='text-detail'>Thiết kế giao diện web </br> Thiết kế web </br> Thiết kế web cho thiết bị di động </br> React Native cơ bản </br> Lập trình Android.</span>";
+//
+document.getElementById("title_experience").innerHTML = "Hoạt động và Kinh nghiệm";
+document.getElementById("experience").innerHTML =
+    "<span class='text-title2'>Tổ chức sự kiện</br></span>" +
+    "<span class='text-detail'>- Tham gia dự án Khi Môi trường lên tiếng - vai trò Huấn luyện viên do trường THCS - Đinh Thiện Lý tổ chức</br></span>" +
+    "<span class='text-title2'>Truyền thông</br></span>" +
+    "<span class='text-detail'>- Tham gia tổ chức sự kiện, truyền thông và dẫn trương trình về giáo dục Môi trường thuộc đội Tuyên truyền Môi truồng (Đoàn khoa Môi trường - ĐH KHTN - TpHCM) với sự tài trợ của công ty Suntory Pepsico kết hợp tring tâm Live&Learn phối hợp thực hiện.</br></span>" +
+    "<span class='text-detail'>- Trưởng ban truyền thông và quản lý fanpage đội Tuyên truyền Môi trường (Đoàn khoa Môi trường - ĐH KHTN -TpHCM).</br></span>" +
+    "<span class='text-title2'>Viết lách online</br></span>" +
+    "<span class='text-title2'>Kinh nghiệp lập trình viên</br></span>" +
+    "<span class='text-detail'>Kinh nghiệm hai năm làm lập trình viên tại công ty Cổ phần Đầu tư và Phát triển Công nghệ thông minh (STID - VTCsmarttech).</span>"
+
+//
+document.getElementById("title_skill").innerHTML = "Các kỹ năng";
+document.getElementById("skill").innerHTML =
+    "<span class='text-title2'>Kỹ năng chuyên ngành</br></span>" +
+    "<span class='text-detail'>Sử dụng tốt các phần mềm chuyên ngành như ArcGIS, ENVI, Mapinfo, SQL Server. Phần mème mô hình hóa như: SWAT, HEC-RAS.</br></span>" +
+    "<span class='text-detail'>Phần mềm chuyên về lập trình và thiết kế: Dreamweaver, Subline Text, Illustrator, Photoshop.</br></span>" +
+    "<span class='text-detail'>Nền tảng về thuật toán trong lập trình: font-end(HTML 5, CSS 3, Bootstrap 3, UI/UX), Android, framwork Angular.</br></span>" +
+    "<span class='text-title2'>Tin học văn phòng</br></span>" +
+    "<span class='text-detail'>Có thể sử dụng tốt các phần mền như Word, Excel, Photoshop.</br></span>" +
+    "<span class='text-title2'>Hoạt động nhóm</br></span>" +
+    "<span class='text-detail'>Có khả năng quản lý và hoạt động nhóm.</span>"
+    //
+document.getElementById("title_award").innerHTML = "Giải thưởng";
+document.getElementById("award").innerHTML =
+    "Đạt top 9 Cuộc thi Tìm hiểu Giải pháp, Sản phẩm TPHCM GIS 2018.</br>" +
+    "Hạng 3 Giải thưởng sinh viên Nghiên cứu Khoa học Eureka lần XXI 2019.</br>" +
+    "Đồng tác giả trong Nghiên cứu Khoa học được đăng trên tạp chí Earth Systems and Environment."
